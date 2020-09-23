e70c51eadf9fde71ba08ea16087872caee8523b7
J2b2590 <J2b2590@gmail.com>
2020-09-23 14:05:59 -0500

    finished lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+("Mr.Chocolate",12,"M","Green","cool",False),
+("Rowdy",12,"M","Green","cool",False),
+("Tabitha",12,"M","Green","cool",False),
+("Sergeant",12,"M","Green","cool",False),
+("Melissa",12,"M","Green","cool",False),
+("Grinch",12,"M","Green","cool",False),
+("Wendy",12,"M","Green","cool",False),
+(NULL,12,"M","Green","cool",False);
